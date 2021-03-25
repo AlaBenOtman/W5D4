5350d4138b8fe0b3ddb4d3bf595bf769ad0454da
voldemort <alabenothman@gmail.com>
2021-03-25 14:18:13 -0700

    applied association methods

--- skeleton/app/models/enrollment.rb
+++ skeleton/app/models/enrollment.rb
@@ -1,14 +1,23 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
- belongs_to :courses,
+ belongs_to :course,
+ primary_key: :id, 
+ foreign_key: :course_id,
+    class_name: 'Course'
 
- primary_key: :id 
- foreign_key: :course_id
- foreign_key: :student_id
-    class_name: :Courses
-
-    belongs_to :users
-    primary_key: :id
-    foreign_key: :student_id
-    class_name: :Users 
+    belongs_to :user,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: 'User'
+    
     
 end
