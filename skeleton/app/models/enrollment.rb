1b28b2ea638fafc96ff604e0d2c1a8925ad5adc5
Elvin Marseille <elvinmarseille@yahoo.com>
2021-03-25 14:27:46 -0400

    enroll

--- skeleton/app/models/enrollment.rb
+++ skeleton/app/models/enrollment.rb
@@ -1,2 +1,14 @@
 class Enrollment < ApplicationRecord
+ belongs_to :courses,
+
+ primary_key: :id 
+ foreign_key: :course_id
+ foreign_key: :student_id
+    class_name: :Courses
+
+    belongs_to :users
+    primary_key: :id
+    foreign_key: :student_id
+    class_name: :Users 
+    
 end
